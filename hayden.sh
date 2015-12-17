700df4ea5e5eed876eb85cb5382268b477579e7f
smokingwheels <smokingwheels@users.noreply.github.com>
2015-12-17 09:13:22 +0800

    For hayden's Minecraft Server
    To stop OPS Hackers changing or adding things to you server.

---
+++ hayden.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#Add to Crontab
+#crontab -e
+#*/1 * * * * /home/john/hayden.sh
+cd /home/john/minecraft
+for i in {1..13}
+do
+cp ops.json.txt ops.json
+cp whitelist.json.txt whiteist.json
+cp server.properties.txt server.properties
+cp banned-ips.json.txt banned-ips.json
+cp banned-players.json.txt banned-players.json
+sleep 5
+echo For Haydens Mincraft Server By Smokingwheels 2015 
+done
